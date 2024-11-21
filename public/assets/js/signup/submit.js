36022ce92a8c8ac33a90def64e4dcce15e1dad83
Falling10fruit <budiyoga09nur@gmail.com>
2024-11-21 19:55:27 +0700

    SIGNNN UP PAGE SEVEN DAYS LEFT WHO CARES ABOUT SECURITY

--- public/assets/js/signup/submit.js
+++ public/assets/js/signup/submit.js
@@ -1,5 +1,34 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js';
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAvqMtjskZ_a8JkM61VJQ_h4iArBwFNnZ0",
+    authDomain: "simplyatmwardaya.firebaseapp.com",
+    projectId: "simplyatmwardaya",
+    storageBucket: "simplyatmwardaya.firebasestorage.app",
+    messagingSenderId: "1073528834240",
+    appId: "1:1073528834240:web:2dc4df309693940e938525"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 document.getElementById("submitButton").onclick = () => {
     const input = document.getElementById("pinInput").value;
 
-    console.log(input);
+    if (input.length == 4 && input%1 == 0) {
+        const accountNumber = Math.floor(Math.random()*10**8);
+
+        setDoc(doc(db, "userInfo", "account" + accountNumber), {
+            PIN: input*1,
+            balance: 0
+        }).then(() => {
+
+            localStorage.setItem("accountNumber", accountNumber);
+            localStorage.setItem("PIN", input*1);
+
+            window.location.href = "../mainmenu";
+        });
+    }
 }
