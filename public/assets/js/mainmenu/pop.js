b6918cd8ccb936eb5073bf7d6990322939cd0f4a
Falling10fruit <budiyoga09nur@gmail.com>
2024-11-27 18:53:27 +0700

    sign up desktop done

--- public/assets/js/mainmenu/pop.js
+++ public/assets/js/mainmenu/pop.js
@@ -17,10 +17,10 @@ function pop () {
 
 function setScale (progress) {
     for (let i = 0; i < options.length; i++) {
-        const x = progress - i/50;
+        const x = progress - i/10;
         const curve = Math.cos(5*(x - 1)*Math.PI);
-        const damp = 2**(-10*x);
+        const damp = 2**(-5*x);
 
-        options[i].style.transform = "scale(" + (x < 1 ? (curve*damp + 1) : 1) + ")";
+        options[i].style.transform = "scale(" + (x < 0 ? 0 : (curve*damp +1)) + ")";
     }
 }
