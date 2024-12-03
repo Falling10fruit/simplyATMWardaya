f8666d831bf91f34d3795bd8718774995f52890b
Falling10fruit <budiyoga09nur@gmail.com>
2024-12-03 09:41:32 +0700

    404 desktop done

--- public/assets/js/404/index.js
+++ public/assets/js/404/index.js
@@ -5,10 +5,20 @@ setInterval(updateMessage, 1000);
 
 function updateMessage () {
     countdown--
-    announce.innerText = "try again in... " + countdown;
 
-    if (countdown < 1) {
-        window.location.href = "../";
-        announce.innerText = "redirecting to welcome page"
+    if (window.innerWidth < 1000) {
+        announce.innerHTML = "try again in... " + countdown;
+    
+        if (countdown < 1) {
+            window.location.href = "../";
+            announce.innerHTML = "redirecting to welcome page"
+        }
+    } else {
+        announce.innerHTML = "Try again in ... " + countdown;
+    
+        if (countdown < 1) {
+            window.location.href = "../";
+            announce.innerHTML = "Redirecting to welcome page"
+        }
     }
 }
